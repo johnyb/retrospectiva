ad97a17bc956f3c8ed63c124129d6225f6a449e7
Julian Bäume <julian@svg4all.de>
2010-09-09 01:52:16 +0200

    move some rails initializition into boot.rb
    see also this blog post:
    http://hone.heroku.com/rails/2010/02/02/rails-23-and-bundler.html

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -19,16 +19,3 @@ rescue Bundler::GemNotFound
     "Did you run `bundle install`?"
 end
 
-class Rails::Boot
-  def run
-    load_initializer
-
-    Rails::Initializer.class_eval do
-      def load_gems
-        @bundler_loaded ||= Bundler.require :default, Rails.env
-      end
-    end
-
-    Rails::Initializer.run(:set_load_path)
-  end
-end
